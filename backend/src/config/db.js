e55226c0d19b8882d60fa08c778db777924cc298
Mustafa Örsçelik <mustafaorscelik@gmail.com>
2025-11-28 19:57:18 +0300

    db models crated

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+
+import mongoose from 'mongoose';
+import { ENV } from './env.js';
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(ENV.MONGO_URI);
+    console.log('MongoDB connected successfully ✅')
+    
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
+    process.exit(1);
+  }
+}
